d1ff585fcdd5b49dfd80a641d97dc73b0e4412c4
Zohar Peled <peled.zohar@gmail.com>
2024-07-10 19:08:16 +0300

    Added test projects

---
+++ AutoEntityGenerator.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace AutoEntityGenerator.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
