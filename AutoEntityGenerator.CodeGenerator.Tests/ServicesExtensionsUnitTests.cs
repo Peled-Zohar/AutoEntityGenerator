d1b77280467d349dd6570e15b659a378e9cf097a
Zohar Peled <peled.zohar@gmail.com>
2025-06-07 14:54:01 +0300

    Added CodeGenerator ServiceExtensions unit test

---
+++ AutoEntityGenerator.CodeGenerator.Tests/ServicesExtensionsUnitTests.cs
@@ -0,0 +1,23 @@
+﻿using AutoEntityGenerator.Common.Interfaces;
+using FakeItEasy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoEntityGenerator.CodeGenerator.Tests
+{
+    class ServicesExtensionsUnitTests
+    {
+        [Test]
+        public void AddCodeGenerator_ShouldRegisterCodeGeneratorServices()
+        {
+            var services = A.Fake<IServices>();
+            services.AddCodeGenerator();
+            A.CallTo(() => services.AddSingleton<IEntityGenerator, EntityGenerator>()).MustHaveHappened();
+            A.CallTo(() => services.AddSingleton<IMappingsClassGenerator, MappingsClassGenerator>()).MustHaveHappened();
+            A.CallTo(() => services.AddSingleton<ICodeFileGenerator, CodeFileGenerator>()).MustHaveHappened();
+        }
+    }
+}
